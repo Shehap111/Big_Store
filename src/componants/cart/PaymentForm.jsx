cc7be54d6728e06ab61e65292ec83f4dfbdc8b64
Shehapsamer <https://github.com/Shehapsamer>
2025-03-18 00:07:31 +0200

    Updated frontend with new CORS settings and Stripe URLs and vircelJson

--- src/componants/cart/PaymentForm.jsx
+++ src/componants/cart/PaymentForm.jsx
@@ -197,10 +197,7 @@ const handleCheckout = async () => {
       navigate("/Profile/orders");
       window.location.reload();
     } else if (paymentMethod === "online") {
-const API_BASE_URL = process.env.NODE_ENV === "production"
-  ? "https://your-live-server.com"
-  : "http://localhost:4242";
-     const response = await fetch(`${API_BASE_URL}/checkout-success`, {
+const response = await fetch("https://big-store-server-olat2aha4-shehaps-projects-baaa1b50.vercel.app/create-checkout-session", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
