32c7e02cb2d98010f93603bc41113eff91222c7d
Shehapsamer <https://github.com/Shehapsamer>
2025-03-18 01:52:39 +0200

    Updated frontend with new CORS settings and Stripe URLs and vircelJson

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: ["es2022", "chrome100", "safari15"], // تحديث لدعم top-level await
-  },
+    target: 'esnext', // تأكد من أنها esnext
+    polyfillModulePreload: false, // تعطيل الـ polyfill لتجنب الأخطاء
+  }
 });
