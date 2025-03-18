e800ec234cfc2efad540057300e30b4b262d9114
Shehapsamer <https://github.com/Shehapsamer>
2025-03-18 02:23:46 +0200

    Updated frontend with new CORS settings and Stripe URLs and vircelJson

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // تأكد إن base مظبوط
+  base: "/", // مهم عشان يحل مشكلة المسارات بعد الرفع
   build: {
     target: 'esnext',
     polyfillModulePreload: false,
     rollupOptions: {
+      input: 'index.html',
       output: {
         entryFileNames: "assets/[name].js",
         chunkFileNames: "assets/[name].js",
