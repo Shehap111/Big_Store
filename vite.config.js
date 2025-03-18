d8129bce350800623bf9ae75289859a31067aae8
Shehapsamer <https://github.com/Shehapsamer>
2025-03-18 02:13:45 +0200

    Updated frontend with new CORS settings and Stripe URLs and vircelJson

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   build: {
     target: 'esnext', // تأكد من أنها esnext
     polyfillModulePreload: false, // تعطيل الـ polyfill لتجنب الأخطاء
+  },
+  server: {
+    historyApiFallback: true, // حل مشكلة الريفريش وإعادة التوجيه للـ index.html
+  },
+  preview: {
+    historyApiFallback: true, // يضمن نفس السلوك عند تشغيل preview
   }
 });
